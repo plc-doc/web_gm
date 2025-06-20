9002e2cf7ee3d363c383b01c440853c34ed2fdae
AlexM <77628445+aalexmalashenko@users.noreply.github.com>
2025-06-20 14:44:11 +0300

    Update clock_view.py

--- clock_view.py
+++ clock_view.py
@@ -17,6 +17,7 @@ class ClockView:
     def __init__(self, app, page):
         self.app = app
         self.page = page
+        self.task = None
 
         self.date_field = flet.TextField(value= self.get_date(),border= flet.InputBorder.NONE, color= "black")
         self.time_field = flet.TextField(border= flet.InputBorder.NONE, color="black")
@@ -86,7 +87,7 @@ class ClockView:
         #     padding=20
         # )
 
-        self.page.add(flet.Container(flet.Column(
+        self.container = flet.Container(flet.Column(
             controls=[
                 flet.Text("Настройка даты и времени", text_align=flet.alignment.center,color=orange,size=20),
                 flet.Container(
@@ -110,7 +111,7 @@ class ClockView:
                             flet.FilledTonalButton(text="Синхронизировать с компьютером",
                                                   bgcolor=orange,
                                                   color="black",
-                                                  on_click=lambda e: print()
+                                                  on_click=lambda e: self.stop
                                                    )
                         ], alignment=flet.MainAxisAlignment.SPACE_EVENLY),
                         flet.VerticalDivider(width= 946, color= "#ACACAC"),
@@ -146,9 +147,12 @@ class ClockView:
             ),
             expand=True,
             padding=20
-        ))
+        )
 
-        # self.get_time()
+        self.get_time()
+
+    def stop(self):
+        self.task = None
 
     def get_date(self):
         # date = datetime.date.today().strftime("%d.%m.%Y")
@@ -165,14 +169,13 @@ class ClockView:
         async def update_time():
             # global time
             while True:
-                # time_now = datetime.datetime.now()
+                time_now = datetime.datetime.now()
 
-                self.time_field.value = f"{now.hour:02d}:{now.minute:02d}:{now.second:02d}"
-                self.page.update()
-
+                self.time_field.value = f"{time_now.hour:02d}:{time_now.minute:02d}:{time_now.second:02d}"
+                self.app.page.update()
                 await asyncio.sleep(1)
 
-        self.page.run_task(update_time)
+        self.task = self.page.run_task(update_time)
 
 
 
