2863b36d4cd104c5938ae5cafe6ce195076d9cac
AlexM <77628445+aalexmalashenko@users.noreply.github.com>
2025-06-20 13:59:58 +0300

    Update app_layout.py

--- app_layout.py
+++ app_layout.py
@@ -1,3 +1,5 @@
+import asyncio
+from datetime import datetime
 from ipaddress import ip_address
 
 import flet
@@ -150,7 +152,21 @@ class AppLayout(flet.Row):
 
         self.controls = [self.sidebar, flet.VerticalDivider(width=0, color= "#CACACA"), self.active_view]
 
-        self.clock_view.get_time()
+        def get_time():
+            now = datetime.now()
+            async def update_time():
+                # global time
+                while True:
+                    # time_now = datetime.datetime.now()
+
+                    self.clock_view.time_field.value = f"{now.hour:02d}:{now.minute:02d}:{now.second:02d}"
+                    self.page.update()
+
+                    await asyncio.sleep(1)
+
+            self.page.run_task(update_time)
+
+            get_time()
 
     # def get_ip4(self):
     #     result = subprocess.run(["ifconfig eth2| grep 'inet' | cut -d: -f2 | awk '{print $2}'"], shell=True,
